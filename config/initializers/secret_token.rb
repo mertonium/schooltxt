bb89facabbeb41a8c847bce1edbde4f9eb7f931a
John Mertens <john.mertens@gmail.com>
2011-08-04 19:29:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schooltxt::Application.config.secret_token = '32f607491dbdcfe1171c6c56006600e10cc8a1b48378621c23a7d7cc10c1e9f048de4bec7e6ee539a607bf234f83d047372c8c6c6f3f5ad94afc5cde35a5b043'
